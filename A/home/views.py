76b043264451f208e77d87ee6c1a20a30d8b67dd
charlie <sadegh.rez@gmail.com>
2021-06-01 17:49:20 +0430

    2-response

--- A/home/views.py
+++ A/home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+#@api_view(['GET','POST'])
+# default [GET]
+@api_view() 
+def one(request):
+    data = {
+        "name":"charlie"
+    }
+    return Response(data)
